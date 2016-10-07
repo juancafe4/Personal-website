ed830a6b393e2eda23ee5d74cb4eaebaeb3ff9ff
Juan Carlos <juancafe2@gmail.com>
2016-10-07 01:13:38 -0400

    Scroll npm installed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,7 @@ class Projects extends React.Component {
 
         			<div className="row">
         				<div className="col-xs-12 col-md-3">
-        					<Thumbnail style={styleThumbnail} href="#" alt="RemindME" src="https://image.freepik.com/free-icon/macos-platform_318-33076.png">
+        					<Thumbnail style={styleThumbnail} href="#" alt="RemindME" src="../../assets/remindMe.png">
         							<h3 style={{color: "white", textTransform: "none"}}>RemindME</h3>
         							<ul className="list-inline banner-social-buttons">
         								<li>
@@ -38,7 +38,7 @@ class Projects extends React.Component {
         					</Thumbnail>
         				</div>
         				<div className="col-xs-12 col-md-3">
-        					<Thumbnail style={styleThumbnail} href="#" alt="PolyBookShare" src="https://cnet4.cbsistatic.com/hub/i/2011/10/27/a66dfbb7-fdc7-11e2-8c7c-d4ae52e62bcc/android-wallpaper5_2560x1600_1.jpg">
+        					<Thumbnail style={styleThumbnail} href="#" alt="PolyBookShare" src="../../assets/polyBookShare.jpg">
         							<h3 style={{color: "white", textTransform: "none"}}>PolyBookShare</h3>
         							<ul className="list-inline banner-social-buttons">
         								<li>
@@ -76,14 +76,19 @@ class Projects extends React.Component {
         							<h3 style={{color: "white", textTransform: "none"}}>BookShare</h3>
         							<ul className="list-inline banner-social-buttons">
         								<li>
-        									<RaisedButton
-					        					style={style}
-					        					className="social"
-					                	label={<i style={{color: "#c7d9e1", fontSize:"xx-large"}}className="fa fa-github" aria-hidden="true"></i>}
-					               		labelColor="#c7d9e1"
-					                  backgroundColor="#05080b" 
-					                  href="https://github.com/FigsAndRice/BookShare-App"
-			                		/>
+        									<a target="_blank" href="https://github.com/FigsAndRice/BookShare-App" >
+	        									<RaisedButton
+						        					style={style}
+						        					className="social"
+						                	label={<i style={{color: "#c7d9e1", fontSize:"xx-large"}}className="fa fa-github" aria-hidden="true"></i>}
+						               		labelColor="#c7d9e1"
+						               		href="https://github.com/FigsAndRice/BookShare-App"
+						                  backgroundColor="#05080b" 
+						                  
+				                		/>
+
+        									</a>
+        									
         								</li>
         							</ul>
         					</Thumbnail>
